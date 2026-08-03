ef6903bf8443a5478c3bf9e255c4005d6fd5bd02
kacprinho <kkawecki298@gmail.com>
2026-08-03 11:58:40 +0100

    Training and evaluating our models

--- src/model_evaluation.py
+++ src/model_evaluation.py
@@ -1,8 +1,7 @@
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 import pandas as pd
 
-#Return the evaluation of the models
-
+#Return the evaluation of the models without cross-validation
 def train_and_evaluate(models, X_train, y_train, X_test, y_test):
     """
     Fits and evaluates given ML models.
@@ -15,7 +14,7 @@ def train_and_evaluate(models, X_train, y_train, X_test, y_test):
     #Create empty scores dictionary for each model
     model_scores = {}
 
-    for name,model in models.items:
+    for name,model in models.items():
 
         #Fit model
         model.fit(X_train, y_train)
@@ -27,14 +26,6 @@ def train_and_evaluate(models, X_train, y_train, X_test, y_test):
         mse = mean_squared_error(y_test, y_pred)
         mae = mean_absolute_error(y_test, y_pred)
         r2 = r2_score(y_test, y_pred)
-
-        print(f"MSE: {mse:.3f}")
-        print(f"MAE: {mae:.3f}")
-        print(f"R^2: {r2:.3f}")
-
-        #Inspect coefficients (interpretability bonus)
-        coeffs = pd.Series(model.coef_, index=X_train.columns)
-        print(coeffs.sort_values(ascending=False))
 
         scores = {
             "MSE": mse,
